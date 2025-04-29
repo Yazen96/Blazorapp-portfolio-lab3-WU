192cc88b32556b2f26abfbe12e6f47ed8680460a
Yazen96 <yazan.alnsierat@iths.se>
2025-04-29 11:56:17 +0200

    Refactor code structure for improved readability and maintainability

---
+++ Blazorapp-portfolio-lab3-WU/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazorapp_portfolio_lab3_WU.Models;
+
+public partial class Project
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? Link { get; set; }
+}
---
+++ Blazorapp-portfolio-lab3-WU/Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazorapp_portfolio_lab3_WU.Models;
+
+public partial class Skill
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int YearsOfExperience { get; set; }
+
+    public string SkillLevel { get; set; } = null!;
+}
